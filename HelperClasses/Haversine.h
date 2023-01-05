6885703bec9ee19eeb73a820ccdcd511f9464f5c
limapedro12 <lima.pedro.12@gmail.com>
2023-01-05 18:55:35 +0000

    Adicionada a documentacao de Haversine

--- HelperClasses/Haversine.h
+++ HelperClasses/Haversine.h
@@ -3,7 +3,29 @@
 
 #include <cmath>
 using namespace std;
+
+/**
+ * @brief Função que retorna uma distancia entre dois pontos
+ *
+ * Esta função retorna uma distancia entre dois pontos, calculada com a formula de Haversine, dadas as suas coordenadas
+ *
+ * @param lat1 Latitude do primeiro ponto
+ * @param lon1 Longitude do primeiro ponto
+ * @param lat2 Latitude do segundo ponto
+ * @param lon2 Longitude do segundo ponto
+ * @return A distancia entre os dois pontos
+ */
 double haversine(double lat1, double lon1, double lat2, double lon2);
+
+/**
+ * @brief Função que retorna uma distancia entre dois pontos
+ *
+ * Esta função retorna uma distancia entre dois pontos, calculada com a formula de Haversine, dadas as suas coordenadas
+ *
+ * @param p1 Par de coordenadas do primeiro ponto
+ * @param p2 Par de coordenadas do segundo ponto
+ * @return A distancia entre os dois pontos
+ */
 double haversine(pair<double, double> p1, pair<double, double> p2);
 
 #endif //FLIGHTS_HAVERSINE_H
