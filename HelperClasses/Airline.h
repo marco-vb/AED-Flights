0635fb5b443d1e3cd3bc900390dfa6f342e7787c
limapedro12 <lima.pedro.12@gmail.com>
2023-01-05 19:02:11 +0000

    Adicionada a documentacao da Airline e do Aeroporto

--- HelperClasses/Airline.h
+++ HelperClasses/Airline.h
@@ -5,19 +5,72 @@
 
 using namespace std;
 
+/**
+ * @brief Classe que representa uma companhia aerea
+ *
+ * Esta classe representa uma companhia aerea, com o seu nome, codigo, call sign e o seu pais de origem
+ */
 class Airline {
 private:
+
+    /// Nome da companhia aerea
     string code;
+
+    /// Codigo da companhia aerea
     string name;
+
+    /// Call sign da companhia aerea
     string callSign;
+
+    /// Pais de origem da companhia aerea
     string country;
 
 public:
+
+    /**
+     * @brief Construtor da classe Airline
+     *
+     * @param code Codigo da companhia aerea
+     * @param name Nome da companhia aerea
+     * @param callSign Call sign da companhia aerea
+     * @param country Pais de origem da companhia aerea
+     */
     Airline(string code, string name, string callSign, string country);
+
+    /**
+     * @brief Retorna o codigo da companhia aerea
+     *
+     * @return O codigo da companhia aerea
+     */
     string getCode() const;
+
+    /**
+     * @brief Retorna o nome da companhia aerea
+     *
+     * @return O nome da companhia aerea
+     */
     string getName() const;
+
+    /**
+     * @brief Retorna o call sign da companhia aerea
+     *
+     * @return O call sign da companhia aerea
+     */
     string getCallSign() const;
+
+    /**
+     * @brief Retorna o pais de origem da companhia aerea
+     *
+     * @return O pais de origem da companhia aerea
+     */
     string getCountry() const;
+
+    /**
+     * @brief Operador de menor
+     *
+     * @param rhs Companhia aerea a comparar
+     * @return True se as companhias aereas se codigo for menor que o codigo da companhia aerea rhs
+     */
     bool operator<(const Airline &rhs) const;
 };
 
--- HelperClasses/Airport.h
+++ HelperClasses/Airport.h
@@ -4,22 +4,79 @@
 #include <string>
 using namespace std;
 
+/**
+ * @brief Classe que representa um aeroporto
+ *
+ * Esta classe representa um aeroporto, com o seu nome, codigo, cidade, pais, latitude e longitude
+ */
 class Airport {
 private:
+    /// Codigo do aeroporto
     string Code;
+    /// Nome do aeroporto
     string Name;
+    /// Cidade do aeroporto
     string City;
+    /// Pais do aeroporto
     string Country;
+    /// Latitude do aeroporto
     double Latitude;
+    /// Longitude do aeroporto
     double Longitude;
 
 public:
+    /**
+     * @brief Retorna o codigo do aeroporto
+     *
+     * @return O codigo do aeroporto
+     */
     string getCode() const;
+
+    /**
+     * @brief Retorna o nome do aeroporto
+     *
+     * @return O nome do aeroporto
+     */
     string getName() const;
+
+    /**
+     * @brief Retorna a cidade em que se encontra  aeroporto
+     *
+     * @return A cidade em que se encontra  aeroporto
+     */
     string getCity() const;
+
+    /**
+     * @brief Retorna o pais em que se encontra aeroporto
+     *
+     * @return O pais em que se encontra aeroporto
+     */
     string getCountry() const;
+
+    /**
+     * @brief Retorna a latitude do aeroporto
+     *
+     * @return A latitude do aeroporto
+     */
     double getLatitude() const;
+
+    /**
+     * @brief Retorna a longitude do aeroporto
+     *
+     * @return A longitude do aeroporto
+     */
     double getLongitude() const;
+
+    /**
+     * @brief Construtor da classe Airport
+     *
+     * @param code Codigo do aeroporto
+     * @param name Nome do aeroporto
+     * @param city Cidade do aeroporto
+     * @param country Pais do aeroporto
+     * @param latitude Latitude do aeroporto
+     * @param longitude Longitude do aeroporto
+     */
     Airport(string code, string name, string city, string country, double latitude, double longitude);
 };
 
